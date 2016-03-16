ded1fb9588cf9730af52afe77e7e3787224d5301
Filipe Abreu <filabreu@gmail.com>
2016-03-16 15:25:37 -0300

    Render table with week days and hours. Add helper to return interval of booking hours. Add helper to create a Time object from day and hour. Add helper to show a number with hour format.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,4 +4,16 @@ module HomeHelper
     Date.today.beginning_of_week..Date.today.end_of_week - 2
   end
 
+  def booking_hours
+    6..23
+  end
+
+  def booking_datetime(date, hour)
+    Time.new(date.year, date.month, date.day, hour)
+  end
+
+  def number_to_hour(hour)
+    "%02d:00" % hour
+  end
+
 end
